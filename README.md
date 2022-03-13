99e2835374729cf2a7d97278dd16c474ac6e88c3
Wallace <wallystudyhard@gmail.com>
2022-03-13 18:46:06 -0300

    projec created

---
+++ README.md
@@ -0,0 +1,13 @@
+# Criar projeto por Yarn
+
+# Instalar pacote Yarn
+## npm install --global yarn
+## yarn create react-app frontend --template typescript
+## Ou
+### npm creat-react-app frontend --template typescript
+
+## yarn start
+
+
+# Backend
+# Gerar ssh key (/c/Users/blach/.ssh/id_ed25519):
---
+++ backend/src/main/java/com/agency/rvrent/RvrentApplication.java
@@ -0,0 +1,13 @@
+package com.agency.rvrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvrentApplication.class, args);
+	}
+
+}
